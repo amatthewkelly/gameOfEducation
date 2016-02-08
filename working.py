41e2f91706fe02575836133c490e4c336d826142
Ethan Kay <elk941@gmail.com>
2016-02-08 00:38:16 -0600

    moved variables to two main dictionaries for latter convenience

--- working.py
+++ working.py
@@ -7,35 +7,36 @@ class Neighborhood(object):
 	"""docstring for Neighborhood"""
 	def __init__(self):
 		super(Neighborhood, self).__init__()
-		# self.nl = open('Data Files/neighborHoodLayout.txt').read()
-		self.nl = [10, 10]  # this is our neighborhood layout (grid)
-		self.cityGrid = [3, 5]  # this is our city layout (grid)
-		# the below are racial diversity thresholds. The first variable in each is the amount
-		# of diversity you require to live in that space if you care about diversity (positive diversity), and the latter 
-		# is the level of diversity at which you will leave if you don't care about diversity (negative diversity).
-		self.diverseReqToLeave = [.2, .5]
-		self.diverseReqToEnter = [.2, .5]
-		self.uniqID = 0  # iterate to give people unique ID #s
-		self.percDiversCare = 0.0  # percentage of people who care about diversity
-		self.racePercentages = [.33, .33]  # white, black, the rest are empty spots
-		self.wealthIndexPeakByRace = [6, 4]  # white, black (out of 10, 9 heightest 0 lowest)
-		# the below distributions are skewed normal distributions for blacks and whites.
-		self.blackSocDistr = [int(b*100000) for b in [max(np.random.normal(loc=-2)+5, 0) for a in xrange(1000)] if b < 10]
-		self.whiteSocDistr = [int(b*100000) for b in [max(np.random.normal(loc=2)+5, 0) for a in xrange(1000)] if b < 10]
-		self.cityLayout = []
-		streets = [[3, 6], [3, 6]]
-		for a in xrange(self.cityGrid[0]):
-			temp = []
-			for b in xrange(self.cityGrid[1]):
-				temp.append(streets)
-			self.cityLayout.append(temp)
+		# self.cityBuild['neighborHoodLayout'] = open('Data Files/neighborHoodLayout.txt').read()
+		self.cityBuild = {
+			'neighborHoodLayout': [14, 14],  # this is our neighborhood layout (grid)
+			'cityGrid': [3, 5],  # this is our city layout (grid)
+			'streets': [[4, 9], [4, 9]]  # specifies street locations within neighborhoods (row, column)
+			}
+		self.popStats = {
+			# the below are racial diversity thresholds. The first variable in each is the amount
+			# of diversity you require to live in that space if you care about diversity (positive diversity), and the latter 
+			# is the level of diversity at which you will leave if you don't care about diversity (negative diversity).
+			'diverseReqToLeave': [.2, .7],
+			'diverseReqToEnter': [.2, .7],
+			'uniqID': 0,  # iterate to give people unique ID #s
+			'percDiversCare': 0.0,  # percentage of people who care about diversity
+			'racePercentages': [.33, .33],  # white, black, the rest are empty spots
+			'wealthIndexPeakByRace': [6, 4],  # white, black (out of 10, 9 heightest 0 lowest)
+			# the below distributions are skewed normal distributions for blacks and whites.
+			'blackSocDistr': [int(b*100000) for b in [max(np.random.normal(loc=-2)+5, 0) for a in xrange(1000)] if b < 10],
+			'whiteSocDistr': [int(b*100000) for b in [max(np.random.normal(loc=2)+5, 0) for a in xrange(1000)] if b < 10]
+			}
+		# the below places streets into a conveninet format for latter calculations...
+		self.cityLayout = [[self.cityBuild['streets'] for b in xrange(
+			self.cityBuild['cityGrid'][1]
+			)] for a in xrange(self.cityBuild['cityGrid'][0])]
 		self.city = []  # the main array for holding the current population state
-		self.createPopulation(new=True)  # if new is False will use the saved population
-		self.output(self.city)
-		for a in xrange(10):
+		self.createPopulation(new=True)  # if new is False, will use the saved population
+		self.output(self.city) # prints the current state of the city
+		for a in xrange(1):
 			self.moveThroughTime()
-			# self.output(self.city)
-		self.output(self.city)
+		self.output(self.city) # prints the current state of the city
 
 	def createPopulation(self, new=False):
 		layoutIndex = 0
@@ -49,9 +50,9 @@ class Neighborhood(object):
 							streets = self.cityLayout[i][j]
 						cityIndex += 1
 				neighborhood = []
-				for a in xrange(self.nl[0]):
+				for a in xrange(self.cityBuild['neighborHoodLayout'][0]):
 					column = []
-					for b in xrange(self.nl[1]):
+					for b in xrange(self.cityBuild['neighborHoodLayout'][1]):
 						if a not in streets[0] and b not in streets[1]:
 							person  = self.createPerson(a, b, n)  # option for static testing ", self.tester[a][b]"
 						else:
@@ -66,34 +67,34 @@ class Neighborhood(object):
 	def createPerson(self, a, b, neighborhood):
 		x = np.random.random()
 		div = np.random.random()
-		if div < self.percDiversCare:
+		if div < self.popStats['percDiversCare']:
 			careDiv = True
 		else:
 			careDiv = False
-		if x < self.racePercentages[0]:
-			socVal = int(np.floor(np.random.random()*len(self.blackSocDistr)))
-			income = self.blackSocDistr[socVal]
+		if x < self.popStats['racePercentages'][0]:
+			socVal = int(np.floor(np.random.random()*len(self.popStats['blackSocDistr'])))
+			income = self.popStats['blackSocDistr'][socVal]
 			person = {
 				'race': 'black',
 				'careDiv': careDiv,
-				'ID': self.uniqID,
+				'ID': self.popStats['uniqID'],
 				'income': income,
 				'neighborhood': neighborhood,
 				'absLoc': 0
 				}
-		elif x >= self.racePercentages[0] and x < self.racePercentages[0] + self.racePercentages[1]:
-			socVal = int(np.floor(np.random.random()*len(self.whiteSocDistr)))
-			income = self.whiteSocDistr[socVal]
+		elif x >= self.popStats['racePercentages'][0] and x < self.popStats['racePercentages'][0] + self.popStats['racePercentages'][1]:
+			socVal = int(np.floor(np.random.random()*len(self.popStats['whiteSocDistr'])))
+			income = self.popStats['whiteSocDistr'][socVal]
 			person = {
 				'race': 'white',
 				'careDiv': careDiv,
-				'ID': self.uniqID,
+				'ID': self.popStats['uniqID'],
 				'income': income,
 				'neighborhood': neighborhood
 				}
 		else:
 			person = {}
-		self.uniqID += 1
+		self.popStats['uniqID'] += 1
 		return person
 
 	def moveThroughTime(self):
@@ -144,8 +145,8 @@ class Neighborhood(object):
 		test1 = []
 		for row in xrange(r-1, r+2):
 			for col in xrange(c-1, c+2):
-				if row > -1 and row < self.nl[0]:
-					if col > -1 and col < self.nl[1]:
+				if row > -1 and row < self.cityBuild['neighborHoodLayout'][0]:
+					if col > -1 and col < self.cityBuild['neighborHoodLayout'][1]:
 						if row != r or col != c:
 							# print col, row
 							# print 'here', type(self.city[n1][row])
@@ -173,16 +174,16 @@ class Neighborhood(object):
 		percDif = 1-percSame
 		if kind == 'leave':
 			wantToMove = False
-			percSameReqMin = self.diverseReqToLeave[1]
-			percDifReqMin = self.diverseReqToLeave[0]
+			percSameReqMin = self.popStats['diverseReqToLeave'][1]
+			percDifReqMin = self.popStats['diverseReqToLeave'][0]
 			if person['careDiv'] and percDif < percDifReqMin:
 				wantToMove = True
 			elif percSame < percSameReqMin:
 				wantToMove = True
 		elif kind == 'enter':
 			wantToMove = True
-			percSameReqMin = self.diverseReqToEnter[1]
-			percDifReqMin = self.diverseReqToEnter[0]
+			percSameReqMin = self.popStats['diverseReqToEnter'][1]
+			percDifReqMin = self.popStats['diverseReqToEnter'][0]
 			if person['careDiv'] and percDif < percDifReqMin:
 				wantToMove = False
 			elif percSame < percSameReqMin:
@@ -194,9 +195,7 @@ class Neighborhood(object):
 		# 	print n
 		return person
 
-	def output(self, state):
-		# print state
-		# # 
+	def output(self, state, write=False):
 		def personReturn(p, kind):
 			temp = None
 			if kind == 'race':
@@ -209,34 +208,38 @@ class Neighborhood(object):
 				elif p['race'] == 'white':
 					temp = 'W'
 			return temp
-		print 'Number of people:', len([per for neigh in state for row in neigh for per in row if per != {}])
 		curRow = 0
 		pState = []
-		for a in xrange(self.cityGrid[0]):
+		for a in xrange(self.cityBuild['cityGrid'][0]):
 			temp = []
-			for b in xrange(self.cityGrid[1]):
+			for b in xrange(self.cityBuild['cityGrid'][1]):
 				temp.append(state[curRow])
 				curRow += 1
 			pState.append(temp)
-		for nRow in xrange(self.cityGrid[0]):
-			for cRow in xrange(self.nl[0]):
+		master = []
+		for nRow in xrange(self.cityBuild['cityGrid'][0]):
+			for cRow in xrange(self.cityBuild['neighborHoodLayout'][0]):
 				temp = []
-				for nCol in xrange(self.cityGrid[1]):
+				for nCol in xrange(self.cityBuild['cityGrid'][1]):
 					temp2 = []
-					for cCol in xrange(self.nl[1]):
+					for cCol in xrange(self.cityBuild['neighborHoodLayout'][1]):
 						item = pState[nRow][nCol][cRow][cCol]
 						temp2.append(personReturn(item, 'race'))
 					temp.append(' '.join(temp2))
-				print '     '.join(temp)
-				# print ' '.join(temp)
-			if nRow != self.cityGrid[0] - 1:
-				print '\n'.join(['  '.join([
-					'' for a in xrange(self.cityGrid[1]*self.nl[1]+(self.cityGrid[1]-1)*2)
-					])+' ' for a in xrange(2)])
-		print ''
-		print '  '.join(['-' for a in self.city])
-		print ''
-
+				master.append('     '.join(temp))
+			if nRow != self.cityBuild['cityGrid'][0] - 1:
+				master.append('\n'.join(['  '.join([
+					'' for a in xrange(self.cityBuild['cityGrid'][1]*self.cityBuild['neighborHoodLayout'][1]+(self.cityBuild['cityGrid'][1]-1)*2)
+					])+' ' for a in xrange(2)]))
+		if write:
+			print 'write'
+		else:
+			print 'Number of people:', len([per for neigh in state for row in neigh for per in row if per != {}])
+			for row in master:
+				print row
+			print ''
+			print '  '.join(['-' for a in self.city])
+			print ''
 
 
 def main():
